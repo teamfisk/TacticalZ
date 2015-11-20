1a8784b4be35d6fea575daf9700a6cc05519ec94
sippeangelo <holmerg.simon@gmail.com>
2015-11-20 16:06:30 +0100

    Added resource folder for lightweight run-time resources that is also relevant to code, eg. shaders and XML schemas

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -2,26 +2,34 @@
 
 SET DeployLocation=bin\
 
-ECHO Deploying resources to %DeployLocation%
+ECHO Deploying assets to %DeployLocation%
 :: Asset folders
 RMDIR "%DeployLocation%\Models"
-RMDIR "%DeployLocation%\Textures"
-RMDIR "%DeployLocation%\Sounds"
 MKLINK "%DeployLocation%\Models\" "assets\Models" /J
+RMDIR "%DeployLocation%\Textures"
 MKLINK "%DeployLocation%\Textures\" "assets\Textures\" /J
-MKLINK "%DeployLocation%\Sounds\" "assets\Sounds\" /J
-:: Configuration files
-MKLINK "%DeployLocation%\DefaultConfig.ini" "assets\DefaultConfig.ini" /H
+RMDIR "%DeployLocation%\Audio"
+MKLINK "%DeployLocation%\Audio\" "assets\Audio\" /J
+
+ECHO Deploying resources to %DeployLocation%
+:: Schemas
+RMDIR "%DeployLocation%\Schema"
+MKLINK "%DeployLocation%\Schema\" "resources\Schema" /J
 :: Shaders
 RMDIR /S /Q "%DeployLocation%\Shaders"
 MKDIR "%DeployLocation%\Shaders"
-MKLINK "%DeployLocation%\Shaders\OpenGL\" "src\game\Rendering\OpenGL\Shaders\" /J
-MKLINK "%DeployLocation%\Shaders\DirectX\" "src\game\Rendering\DirectX\Shaders\" /J
+MKLINK "%DeployLocation%\Shaders\" "resources\Shaders" /J
+MKLINK "%DeployLocation%\Shaders\" "resources\Shaders" /J
+:: Configuration files
+MKLINK "%DeployLocation%\DefaultConfig.ini" "resources\DefaultConfig.ini" /H
+
 :: Platform specific binaries
 IF "%~1"=="" GOTO :EOF
 ECHO Deploying %1 binaries to %DeployLocation%
 COPY "deps\bin\%1\x64\*.dll" "%DeployLocation%"
+
 :: Licenses
+::ECHO Copying licenses %DeployLocation%
 ::COPY "libs\assimp-3.1.1\LICENSE" "%ConfigPath%\Assimp License.txt"
 ::COPY "libs\glew-1.11.0\LICENSE.txt" "%ConfigPath%\GLEW License.txt"
 ::COPY "libs\glm-0.9.5.4\copying.txt" "%ConfigPath%\GLM License.txt"
