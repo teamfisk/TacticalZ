af294f78b7622ac5ec0ad21faa2ef571bb9fb5c3
verysecrethero <hnorthh@gmail.com>
2015-12-02 16:26:30 +0100

    czdxvxdfvxvxvc

--- src/Engine/Core/Collision.cpp
+++ src/Engine/Core/Collision.cpp
@@ -40,15 +40,13 @@ bool RayAABBIntr(const Ray& ray, const AABB& box)
 bool RayVsAABB(const Ray& ray, const AABB& box)
 {
     glm::vec3 invdir = 1.0f / ray.Direction;
-    glm::vec3 bMin = box.MinCorner();
-    glm::vec3 bMax = box.MaxCorner();
-    
-    float t1 = (bMin.x - ray.Origin.x)*invdir.x;
-    float t2 = (bMax.x - ray.Origin.x)*invdir.x;
-    float t3 = (bMin.y - ray.Origin.y)*invdir.y;
-    float t4 = (bMax.y - ray.Origin.y)*invdir.y;
-    float t5 = (bMin.z - ray.Origin.z)*invdir.z;
-    float t6 = (bMax.z - ray.Origin.z)*invdir.z;
+
+    float t1 = (box.MinCorner().x - ray.Origin.x)*invdir.x;
+    float t2 = (box.MaxCorner().x - ray.Origin.x)*invdir.x;
+    float t3 = (box.MinCorner().y - ray.Origin.y)*invdir.y;
+    float t4 = (box.MaxCorner().y - ray.Origin.y)*invdir.y;
+    float t5 = (box.MinCorner().z - ray.Origin.z)*invdir.z;
+    float t6 = (box.MaxCorner().z - ray.Origin.z)*invdir.z;
 
     float tmin = std::max(std::max(std::min(t1, t2), std::min(t3, t4)), std::min(t5, t6));
     float tmax = std::min(std::min(std::max(t1, t2), std::max(t3, t4)), std::max(t5, t6));
--- src/Tests/CollisionTest.cpp
+++ src/Tests/CollisionTest.cpp
@@ -2,16 +2,72 @@
 using boost::unit_test_framework::test_suite;
 using boost::unit_test_framework::test_case;
 #include <Engine\Core\Collision.h>
+#include <stdlib.h>//srand
 
 BOOST_AUTO_TEST_SUITE(collisionTests)
 
 BOOST_AUTO_TEST_CASE(collisionTest)
 {
+    //fixed seed
+    srand(2);
     Collision::Ray ray;
-    ray.Origin = glm::vec3(0, 0, 0);
-    ray.Direction = glm::vec3(0, 0, 0);
-    auto someAABB = Collision::AABB(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0));
-    bool z = Collision::RayVsAABB(ray, someAABB);
+    Collision::AABB someAABB;
+    glm::vec3 minPos;
+    glm::vec3 maxPos;
+    bool z;
+    int test = 0;
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        ray.Origin.x = rand() % 100;
+        ray.Origin.y = rand() % 100;
+        ray.Origin.z = rand() % 100;
+        ray.Direction.x = rand() % 100;
+        ray.Direction.y = rand() % 100;
+        ray.Direction.z = rand() % 100;
+        minPos.x = rand() % 100;
+        minPos.y = rand() % 100;
+        minPos.z = rand() % 100;
+        maxPos.x = rand() % 100;
+        maxPos.y = rand() % 100;
+        maxPos.z = rand() % 100;
+
+        someAABB = Collision::AABB(minPos, maxPos);
+        z = Collision::RayVsAABB(ray, someAABB);
+        if (z) ++test;
+    }
+    BOOST_CHECK(test >= 0);
+}
+
+BOOST_AUTO_TEST_CASE(collisionTest2)
+{
+    //fixed seed
+    srand(2);
+    Collision::Ray ray;
+    Collision::AABB someAABB;
+    glm::vec3 minPos;
+    glm::vec3 maxPos;
+    bool z;
+    int test = 0;
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        ray.Origin.x = rand() % 100;
+        ray.Origin.y = rand() % 100;
+        ray.Origin.z = rand() % 100;
+        ray.Direction.x = rand() % 100;
+        ray.Direction.y = rand() % 100;
+        ray.Direction.z = rand() % 100;
+        minPos.x = rand() % 100;
+        minPos.y = rand() % 100;
+        minPos.z = rand() % 100;
+        maxPos.x = rand() % 100;
+        maxPos.y = rand() % 100;
+        maxPos.z = rand() % 100;
+
+        someAABB = Collision::AABB(minPos, maxPos);
+        z = Collision::RayAABBIntr(ray, someAABB);
+        if (z) ++test;
+    }
+    BOOST_CHECK(test >= 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
