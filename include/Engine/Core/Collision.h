955c9ce5340327eed2be0a5558c604632e2c8013
William Moberg <william.moberg@live.se>
2015-12-07 16:00:34 +0100

    Removed superfluous function declaration.

--- include/Engine/Core/Collision.h
+++ include/Engine/Core/Collision.h
@@ -12,7 +12,6 @@ bool RayVsAABB(const Ray& ray, const AABB& box);
 bool RayVsAABB(const Ray& ray, const AABB& box, float& outDistance);
 
 bool AABBVsAABB(const AABB& a, const AABB& b);
-bool AABBVsAABBTst(const AABB& a, const AABB& b);
 }
 
 #endif
