48339934b6e2aa50dfc97ba9ee662a469cc774e4
stiffly <adambylehn@hotmail.com>
2015-12-07 14:32:49 +0100

    Created skeleton for Server/Client logic.

---
+++ include/Engine/Networking/Server.h
@@ -0,0 +1,13 @@
+#ifndef Server_h__
+#define Server_h__
+
+#include <boost\asio.hpp>
+
+class Server
+{
+	Server();
+	~Server();
+};
+
+#endif
+
---
+++ src/Engine/Networking/Client.cpp
@@ -0,0 +1,11 @@
+#include "Networking\Client.h"
+
+Client::Client()
+{
+
+}
+
+Client::~Client()
+{
+
+}
