db5b6ad51908cdf1603ec02a4fc00c2675b1150d
Jocke <joachimpetterson@gmail.com>
2015-12-17 17:32:49 +0100

    Added more error code to Packet class

--- src/Engine/Network/Package.cpp
+++ src/Engine/Network/Package.cpp
@@ -16,7 +16,11 @@ Package::Package(MessageType type, unsigned int& packageID)
 Package::Package(char* data, const int sizeOfPackage)
 {
     // Create message
+
+    // Resize message
+    m_MaxPacketSize = sizeOfPackage;
     m_Data = new char[sizeOfPackage];
+    // Copy data newly allocated memory
     memcpy(m_Data, data, sizeOfPackage);
     m_Offset = sizeOfPackage;
 }
@@ -29,14 +33,18 @@ Package::~Package()
 void Package::AddString(std::string str)
 {
     // Message, add one extra byte for null terminator
-    memcpy(m_Data + m_Offset, str.data(), (str.size() + 1) * sizeof(char));
-    m_Offset += (str.size() + 1) * sizeof(char);
+    int sizeOfString = str.size() + 1;
+    if (m_Offset + sizeOfString > m_MaxPacketSize) {
+        LOG_WARNING("Package::AddString(): Data size in packet exceeded maximum package size.\n");
+    }
+    memcpy(m_Data + m_Offset, str.data(), sizeOfString * sizeof(char));
+    m_Offset += sizeOfString * sizeof(char);
 }
 
 void Package::AddData(char * data, int sizeOfData)
 { 
-    if (m_Offset + sizeOfData > 128) {
-        LOG_WARNING("Package::AddData(): Data size in package exceeded maximum package size.\n");
+    if (m_Offset + sizeOfData > m_MaxPacketSize) {
+        LOG_WARNING("Package::AddData(): Data size in packet exceeded maximum package size.\n");
     }
     memcpy(m_Data + m_Offset, data, sizeOfData);
     m_Offset += sizeOfData;
@@ -46,7 +54,7 @@ std::string Package::PopFrontString()
 {
     std::string returnValue(m_Data + m_ReturnDataOffset);
     if (m_Offset < m_ReturnDataOffset + returnValue.size()){
-        LOG_WARNING("Package PopFrontString(): Oh no! You are trying to remove things outside my memory kingdom");
+        LOG_WARNING("packet PopFrontString(): Oh no! You are trying to remove things outside my memory kingdom");
         return "PopFrontString Failed";
     }   
     // +1 for null terminator.
@@ -56,6 +64,10 @@ std::string Package::PopFrontString()
 
 char * Package::PopData(int SizeOfData)
 {
+    if (m_Offset < m_ReturnDataOffset + SizeOfData) {
+        LOG_WARNING("packet PopData(): Oh no! You are trying to remove things outside my memory kingdom");
+        return nullptr;
+    }
     unsigned int oldReturnDataOffset = m_ReturnDataOffset;
     m_ReturnDataOffset += SizeOfData;
     return (m_Data + oldReturnDataOffset);
