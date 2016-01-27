c5b1afb0d493e14fdf7b4e22e8e7cf0514d8f642
antc13 <exlerth@hotmail.com>
2016-01-27 09:37:01 +0100

    Exporter Fix

--- tools/MayaExporter/MayaExporter/Export.cpp
+++ tools/MayaExporter/MayaExporter/Export.cpp
@@ -47,13 +47,24 @@ bool Export::Meshes(std::string pathName, bool selectedOnly)
 
                     for (unsigned int i = 0; i < connections.length(); i++) {
                         if (connections[i].node().apiType() == MFn::kSkinClusterFilter) {
-                            MGlobal::select(shape.parent(i), MGlobal::kReplaceList);
-                            MGlobal::displayInfo(MString() + "Moving " + thisNode.name() + " to bindPose.");
+                            shape.parent(0, &status);
+                            if (status != MS::kSuccess) {
+                                MGlobal::displayError(MString() + "shape.parent(0, &status) failed with: " + status.errorString());
+                            }
+                            status = MGlobal::select(shape.parent(0), MGlobal::kReplaceList);
+                            if (status != MS::kSuccess) {
+                                MGlobal::displayError(MString() + "Parent to " + thisNode.name() + " failed");
+                            }
+
+                            MFnDependencyNode tmp(shape.parent(0));
+                            MGlobal::displayInfo(MString() + "Moving " + tmp.name() + " to bindPose.");
+
                             status = MGlobal::executeCommand("GoToBindPose;");
                             if (status != MS::kSuccess) {
                                 MGlobal::displayError(MString() + "GoToBindPose: " + status.errorString());
                             }
-                            MGlobal::displayInfo(MString() + "Has moved " + thisNode.name() + " to bindPose.");
+
+                            MGlobal::displayInfo(MString() + "Has moved " + tmp.name() + " to bindPose.");
                         }
                     }
                 }
