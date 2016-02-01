3cfbbbbf54c0365bb0244d1f7289970cea4efddb
William Moberg <william.moberg@live.se>
2016-02-01 16:13:48 +0100

    CollisionSystem now utilizes Octree for collision detection.

--- src/Engine/Collision/CollisionSystem.cpp
+++ src/Engine/Collision/CollisionSystem.cpp
@@ -1,6 +1,7 @@
 #include "Collision/Collision.h"
 #include "Collision/CollisionSystem.h"
 #include "Core/AABB.h"
+#include "Rendering/Model.h"
 
 void CollisionSystem::UpdateComponent(EntityWrapper& entity, ComponentWrapper& component, double dt)
 {
@@ -25,33 +26,28 @@ void CollisionSystem::UpdateComponent(EntityWrapper& entity, ComponentWrapper& c
             continue;
         }
 
-        if (Collision::AABBVsAABB(boxA, boxB, resolutionVector)) {
+        if (boxB.Entity.HasComponent("Model") && Collision::AABBVsAABB(boxA, boxB)) {
+            //Here we know boxB is a entity with Collideable, AABB, and Model.
+            RawModel* model;
+            try {
+                model = ResourceManager::Load<RawModel, true>(boxB.Entity["Model"]["Resource"]);
+            } catch (const std::exception&) {
+                continue;
+            }
+
+            glm::mat4 modelMatrix = Transform::ModelMatrix(boxB.Entity);
+
+            glm::vec3 newVelocity = (glm::vec3)cPhysics["Velocity"];
+            if (Collision::AABBvsTriangles(boxA, model->m_Vertices, model->m_Indices, modelMatrix, newVelocity, resolutionVector)) {
+                (glm::vec3&)cTransform["Position"] += resolutionVector;
+                cPhysics["Velocity"] = newVelocity;
+            }
+        } else if (Collision::AABBVsAABB(boxA, boxB, resolutionVector)) {
+            //Enter here if boxB has no Model.
             (glm::vec3&)cTransform["Position"] += resolutionVector;
             if (resolutionVector.y > 0) {
                 ((glm::vec3&)cPhysics["Velocity"]).y = 0.f;
             }
         }
     }
-
-    // HACK: Temporarily collide against all collidable models since they're not in the octree yet
-    //auto otherCollidables = world->GetComponents("Model");
-    //for (auto& cModel : *otherCollidables) {
-    //    if (cModel.EntityID == entity) {
-    //        continue;
-    //    }
-    //    if (!world->HasComponent(cModel.EntityID, "Collidable")) {
-    //        continue;
-    //    }
-
-    //    auto absPosition = RenderQueueFactory::AbsolutePosition(world, cModel.EntityID);
-    //    auto absOrientation = RenderQueueFactory::AbsoluteOrientation(world, cModel.EntityID);
-    //    auto absScale = RenderQueueFactory::AbsoluteScale(world, cModel.EntityID);
-    //    glm::mat4 modelMatrix = glm::translate(absPosition); // *glm::toMat4(absOrientation) * glm::scale(absScale);
-
-    //    auto model = ResourceManager::Load<Model>(cModel["Resource"]);
-    //    glm::vec3 resolutionVector;
-    //    if (Collision::AABBvsTriangles(boxA, model->m_Vertices, model->m_Indices, modelMatrix, resolutionVector)) {
-    //        (glm::vec3&)cTransform["Position"] += resolutionVector;
-    //    }
-    //}
 }
