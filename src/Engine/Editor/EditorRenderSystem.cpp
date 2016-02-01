a188a3f7faaab87c03876dde9011943cbd510a36
Tleety <dahltobbe@hotmail.com>
2016-02-01 18:22:36 +0100

    Removed widget pointlight since it is now redundant

--- src/Engine/Editor/EditorRenderSystem.cpp
+++ src/Engine/Editor/EditorRenderSystem.cpp
@@ -30,7 +30,7 @@ void EditorRenderSystem::Update(double dt)
         //scene.AmbientColor = (glm::vec4)(*cSceneLight->begin())["AmbientColor"];
         m_RenderFrame->Gamma = 2.2;
         m_RenderFrame->Exposure = 1.0;
-        scene.AmbientColor = glm::vec4(0.6, 0.5, 0.5, 1.0);
+        scene.AmbientColor = glm::vec4(0.8, 0.8, 0.8, 1.0);
     }
 
     auto models = m_World->GetComponents("Model");
