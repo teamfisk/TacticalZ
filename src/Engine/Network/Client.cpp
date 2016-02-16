d666bd893d97d925ec332bae247d8314e0278c54
Jocke <joachimpetterson@gmail.com>
2016-02-16 10:03:52 +0000

    fixed bug in Client::parsePlayerDamage

--- src/Engine/Network/Client.cpp
+++ src/Engine/Network/Client.cpp
@@ -437,7 +437,7 @@ void Client::parsePlayerDamage(Packet& packet)
 {
     Events::PlayerDamage e;
     PlayerID victimID = packet.ReadPrimitive<EntityID>();
-    if(serverClientMapsHasEntity(victimID)){
+    if(!serverClientMapsHasEntity(victimID)){
         return;
     }
     e.Inflictor = EntityWrapper(m_World, m_ServerIDToClientID.at(victimID));
