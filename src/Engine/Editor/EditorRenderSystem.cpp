50b0a091ec4354a3d1996d0218de34f1e63a1434
sippeangelo <holmberg.simon@gmail.com>
2016-03-09 22:06:08 +0100

    Fixed editor widgets to take scale of the entity into account when they're moved

--- src/Engine/Editor/EditorRenderSystem.cpp
+++ src/Engine/Editor/EditorRenderSystem.cpp
@@ -7,7 +7,7 @@ EditorRenderSystem::EditorRenderSystem(SystemParams params, IRenderer* renderer,
 {
     EVENT_SUBSCRIBE_MEMBER(m_ESetCamera, &EditorRenderSystem::OnSetCamera);
     auto resolution = Rectangle::Rectangle(1280, 720);
-    m_EditorCamera = new Camera((float)resolution.Width / resolution.Height, glm::radians(45.f), 0.01f, 5000.f);
+    m_EditorCamera = new Camera((float)resolution.Width / resolution.Height, glm::radians(45.f), 0.001f, 500.f);
 }
 
 void EditorRenderSystem::Update(double dt)
