123fbf56333e6096e94fc031fc23c6e34f1733bd
sippeangelo <holmberg.simon@gmail.com>
2016-03-09 23:47:42 +0100

    EEntityDeleted now provides an EntityWrapper instead of just an ID

--- src/Engine/Network/Server.cpp
+++ src/Engine/Network/Server.cpp
@@ -494,7 +494,7 @@ bool Server::OnEntityDeleted(const Events::EntityDeleted & e)
 {
     if (!e.Cascaded) {
         Packet packet = Packet(MessageType::EntityDeleted);
-        packet.WritePrimitive<EntityID>(e.DeletedEntity);
+        packet.WritePrimitive<EntityID>(e.DeletedEntity.ID);
         reliableBroadcast(packet);
     }
     return false;
