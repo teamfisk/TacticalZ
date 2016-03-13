8d809f30f7196f806c1e8a63a3e67b051c8d70dc
Tleety <dahltobbe@hotmail.com>
2016-03-13 02:25:11 +0100

    Muzzleflash now very nice

--- src/Game/Systems/FadeSystem.cpp
+++ src/Game/Systems/FadeSystem.cpp
@@ -15,15 +15,27 @@ void FadeSystem::UpdateComponent(EntityWrapper& entity, ComponentWrapper& cFade,
         dTime *= -1;
     }
 
+    if((bool)cFade["Reverse"]) {
+        dTime *= 2;
+    }
+
     double fadeTime = cFade["FadeTime"];
     double& currentTime = cFade["Time"];
     currentTime += dTime;
 
     if(currentTime > fadeTime) {
         currentTime = 0.0;
+        if ((bool)cFade["Reverse"]) {
+            (bool&)cFade["Out"] = !(bool)cFade["Out"];
+            currentTime = fadeTime;
+        }
     }
     if(currentTime < 0.0) {
         currentTime = fadeTime;
+        if ((bool)cFade["Reverse"]) {
+            (bool&)cFade["Out"] = !(bool)cFade["Out"];
+            currentTime = 0.0;
+        }
     }
     double ratio = currentTime/fadeTime;
 
